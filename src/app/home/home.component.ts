16b2030af4e9bcd80dd2d082fcbdc81fc4f2e9e4
Nael Mezdari <nmz94140@gmail.com>
2022-10-18 11:43:42 +0200

    get file data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,6 +25,29 @@ export class HomeComponent implements OnInit {
 		if (target.files) {
 			this.fileToUpload = target.files[0];
 			console.log(this.fileToUpload);
+			const reader = new FileReader();
+			reader.readAsText(this.fileToUpload);
+			reader.onload = function(event){
+				if (event.target !== null && event.target.result !== null) {
+					const csvData = event.target.result; // Content of CSV file
+					if (typeof csvData === 'string') {
+						let allTextLines = csvData.split(/\r\n|\n/);
+						let headers = allTextLines[0].split(',');
+						let lines = [];
+						for ( let i = 0; i < allTextLines.length; i++) {
+							let data = allTextLines[i].split(',');
+							if (data.length == headers.length) {
+								let line = [];
+								for ( let j = 0; j < headers.length; j++) {
+									line.push(data[j]);
+								}
+								lines.push(line);
+							}
+						}
+						console.log(lines); //The data in the form of 2 dimensional array.
+					}
+				}
+			}
 		}
 	}
 }
