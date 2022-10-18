24dd681296b9e21c3c560d8bdb08fb44e7a1dad0
hugosinp <hugo.anaponn@gmail.com>
2022-10-18 10:40:59 +0200

    added spline

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Application } from '@splinetool/runtime';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const canvas = document.getElementById('canvas3d');
+    const app = new Application(<HTMLCanvasElement>canvas);
+    app.load('https://prod.spline.design/ZZ6xHiy1V3PnvIoZ/scene.splinecode');
   }
 
 }
