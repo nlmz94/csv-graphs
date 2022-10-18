1adc8e6d2f282037bad5b328da2fc52cf1c3285f
Nael Mezdari <nmz94140@gmail.com>
2022-10-18 12:24:52 +0200

    ugly array

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./home.component.sass'],
 })
 export class HomeComponent implements OnInit {
-	fileToUpload: File | null = null;
+	fileToUpload: File | null = null
+	public headers: string[] | undefined
+	public lines: [string[]] | undefined
+	private rawString: string | ArrayBuffer | null | undefined
 
 	constructor() {
 	}
@@ -21,27 +24,27 @@ export class HomeComponent implements OnInit {
 			console.log(this.fileToUpload);
 			const reader = new FileReader();
 			reader.readAsText(this.fileToUpload);
-			reader.onload = function(event){
-				if (event.target !== null && event.target.result !== null) {
-					const csvData = event.target.result; // Content of CSV file
-					if (typeof csvData === 'string') {
-						let allTextLines = csvData.split(/\r\n|\n/);
-						let headers = allTextLines[0].split(',');
-						let lines = [];
-						for ( let i = 0; i < allTextLines.length; i++) {
-							let data = allTextLines[i].split(',');
-							if (data.length == headers.length) {
-								let line = [];
-								for ( let j = 0; j < headers.length; j++) {
-									line.push(data[j]);
-								}
-								lines.push(line);
+			reader.addEventListener("load", () => {
+				this.rawString = reader.result;
+				if (typeof this.rawString === 'string') {
+					this.lines = [[]]
+					this.headers = []
+					let allTextLines = this.rawString.split(/\r\n|\n/);
+					this.headers = allTextLines[0].split(',');
+					for ( let i = 1; i < allTextLines.length; i++) {
+						let data = allTextLines[i].split(',');
+						if (data.length == this.headers.length) {
+							let line = [];
+							for ( let j = 0; j < this.headers.length; j++) {
+								line.push(data[j]);
 							}
+							this.lines.push(line);
 						}
-						console.log(lines); //The data in the form of 2 dimensional array.
 					}
+					console.log(this.headers); //The data headers.
+					console.log(this.lines); //The data in the form of 2 dimensional array.
 				}
-			}
+			}, false);
 		}
 	}
 }
