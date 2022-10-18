a291b94de3d3b5845dafab67844baed68af98ca2
Nael Mezdari <nmz94140@gmail.com>
2022-10-18 11:32:08 +0200

    upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.sass'],
 })
 export class HomeComponent implements OnInit {
+	fileToUpload: File | null = null;
+	public CSVConstants = {
+		tokenDelimeter: ',',
+		isHeaderPresentFlag: true,
+		validateHeaderAndRecordLengthFlag: true,
+		valildateFileExtenstionFlag: true,
+	};
 
-  constructor() { }
+	constructor() {
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	handleFileInput(e: Event) {
+		const target = <HTMLInputElement>e.target;
+		if (target.files) {
+			this.fileToUpload = target.files[0];
+			console.log(this.fileToUpload);
+		}
+	}
 }
